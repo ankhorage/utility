8311af02daa2024cfea8f067c24dcccf64650202
artiphishle <fabio.gartenmann@protonmail.com>
2026-08-31 23:12:18 +0200

    fix: include managed extensions in Knip

--- knip.config.ts
+++ knip.config.ts
@@ -2,5 +2,11 @@ import { createKnipConfig } from '@ankhorage/devtools/knip';
 
 export default createKnipConfig({
   entry: ['src/project/index.ts', 'src/regex/index.ts'],
-  ignoreFiles: ['.prettierrc.js', 'eslint.config.mjs', 'paradox.config.ts'],
+  ignoreFiles: [
+    '.prettierrc.js',
+    'eslint.config.mjs',
+    'eslint.local.config.mjs',
+    'paradox.config.ts',
+    'prettier.local.config.js',
+  ],
 });
