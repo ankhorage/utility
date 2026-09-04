906df493a7d511a71afe5bab2b1e90a8129c15c4
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 02:35:15 +0200

    feat: export scheduling utilities

---
+++ src/scheduling/index.ts
@@ -0,0 +1,11 @@
+export {
+  createCoalescedTask,
+  createLatestAsyncCoordinator,
+  createSettleCoordinator,
+  type CoalescedTask,
+  type LatestAsyncCoordinator,
+  type LatestAsyncResult,
+  type Scheduler,
+  type SettleCoordinator,
+  type SettleCoordinatorOptions,
+} from './scheduling.js';
