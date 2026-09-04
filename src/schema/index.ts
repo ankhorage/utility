7700c8d5545e0c6e2d5f9431cdbfa606845c8500
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 02:44:13 +0200

    feat: export schema utilities

---
+++ src/schema/index.ts
@@ -0,0 +1,9 @@
+export {
+  resolveEffectiveSchemaType,
+  resolveSchemaReference,
+  resolveSingleSchemaType,
+  type EffectiveSchemaType,
+  type SchemaPrimitiveType,
+  type SchemaReferenceLike,
+  type SchemaShape,
+} from './schema.js';
