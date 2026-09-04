e17e3ff5141d774678314e79dac74365d46ddbe7
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 03:13:57 +0200

    feat: export object transforms and traversal

--- src/object/index.ts
+++ src/object/index.ts
@@ -8,3 +8,16 @@ export {
   readOwnProperty,
   setOwnProperty,
 } from './object.js';
+export {
+  withOptionalOwnProperty,
+  withOwnProperty,
+  withoutOwnProperty,
+} from './immutable.js';
+export { findOwnPropertyValue } from './lookup.js';
+export {
+  assertNoNestedKeys,
+  findNestedKey,
+  walkNestedValues,
+  type NestedKeyMatch,
+  type NestedValueVisit,
+} from './nested.js';
