df19cd446ea9d194ee99e2ee5bd7a492ed951d2c
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 04:49:28 +0200

    feat: export path utilities

--- src/node/path/index.ts
+++ src/node/path/index.ts
@@ -2,3 +2,4 @@ export { findAncestorDirectory } from './findAncestorDirectory.js';
 export { resolveModuleRelativePath } from './resolveModuleRelativePath.js';
 export { resolvePackageRoot } from './resolvePackageRoot.js';
 export { toPortablePath } from './toPortablePath.js';
+export { toRelativeImportPath } from './toRelativeImportPath.js';
