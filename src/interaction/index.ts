560598c5983f39dc2dd783b01e88acdd6ced74af
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 03:01:01 +0200

    feat: export interaction utilities

---
+++ src/interaction/index.ts
@@ -0,0 +1,13 @@
+export {
+  createStationarySelectionCoordinator,
+  createStationarySelectionInputState,
+  getInteractionKey,
+  isSupportedPointerInput,
+  type CommitSelectionResult,
+  type InteractionSelectionTransaction,
+  type StationaryPointerInput,
+  type StationarySelectionCoordinator,
+  type StationarySelectionInput,
+  type StationarySelectionInputState,
+  type StationaryTouchInput,
+} from './interaction.js';
