c3ec8602c5d3c35a8e5758adf36aaca6b0573d0b
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 04:49:20 +0200

    feat: export filesystem utilities

--- src/node/fs/index.ts
+++ src/node/fs/index.ts
@@ -1,3 +1,8 @@
 export { isDirectory } from './isDirectory.js';
+export { isDirectoryNotEmptyError } from './isDirectoryNotEmptyError.js';
 export { isMissingPathError } from './isMissingPathError.js';
+export { listFilesRecursive } from './listFilesRecursive.js';
 export { pathExists } from './pathExists.js';
+export { pruneEmptyDirectories } from './pruneEmptyDirectories.js';
+export { removeFileWithinRoot } from './removeFileWithinRoot.js';
+export { writeFileWithinRoot } from './writeFileWithinRoot.js';
