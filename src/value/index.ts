061e1fb22721d1f18fb0b4ccba247c129a8e8ac0
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 05:12:23 +0200

    feat: export array assertion

--- src/value/index.ts
+++ src/value/index.ts
@@ -1,4 +1,5 @@
 export { asFiniteNumber } from './asFiniteNumber.js';
 export { asNonEmptyString } from './asNonEmptyString.js';
 export { asString } from './asString.js';
+export { assertArray } from './assertArray.js';
 export { isOneOf } from './isOneOf.js';
