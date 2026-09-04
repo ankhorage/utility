7de7b350c179bfa3969cd97bcc83b07749965f62
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-04 02:43:06 +0200

    feat: add portable path utility

--- src/node/path/path.ts
+++ src/node/path/path.ts
@@ -30,3 +30,10 @@ export function findAncestorDirectory(
     current = parent;
   }
 }
+
+/***
+ * Convert platform-specific path separators to portable forward slashes.
+ */
+export function toPortablePath(value: string): string {
+  return value.split(path.sep).join('/');
+}
